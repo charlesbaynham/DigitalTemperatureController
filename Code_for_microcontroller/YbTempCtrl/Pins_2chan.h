a4bab576dcdfb1b674188120caabf47e337c25dd
Fara <ssr1@npl.ad.local>
2017-03-21 18:09:34 +0000

    Pins for the 2 channel board

--- Code_for_microcontroller/YbTempCtrl/Pins_2chan.h
+++ Code_for_microcontroller/YbTempCtrl/Pins_2chan.h
@@ -41,23 +41,29 @@ const uint8_t ADC_POW			= 12;		/*!< ADC channel for chip power level measurement
 const uint8_t DAC_CS 			= 9;		/*!< Chip select pin of the DAC on the ATMega */
 const uint8_t OPA_ES1 			= A0;		/*!< E/S pin for first OPA */
 const uint8_t OPA_ES2 			= A1;		/*!< E/S pin for second OPA */
-const bool OPA_1_IS_548			= true;		/*!< Is OPA 1 an OPA548 (instead of an OPA549)? */
-const bool OPA_2_IS_548			= true;		/*!< Is OPA 2 an OPA548 (instead of an OPA549)? */
+const bool OPA_1_IS_548			= false;	/*!< Is OPA 1 an OPA548 (instead of an OPA549)? */
+const bool OPA_2_IS_548			= false;	/*!< Is OPA 2 an OPA548 (instead of an OPA549)? */
 
 // DAC channels
 const uint8_t VPLUS_CHAN_1 	= 1;			/*!< DAC channel for first OPA's V+ input */
 const uint8_t VPLUS_CHAN_2 	= 0;			/*!< DAC channel for second OPA's V+ input */
-const uint8_t VLIM_CHAN_1 	= 2;			/*!< DAC channel for first OPA's Vlim input */
-const uint8_t VLIM_CHAN_2 	= 3;			/*!< DAC channel for second OPA's Vlim input */
+const uint8_t VLIM_CHAN_1 	= 3;			/*!< DAC channel for first OPA's Vlim input */
+const uint8_t VLIM_CHAN_2 	= 2;			/*!< DAC channel for second OPA's Vlim input */
 
 // OPA settings
 extern double MAX_VOLTAGE;					/*!< Maximum voltage for the OPAs to output. Calculated in setup() */
 const double MAX_INITIAL_CURRENT = 2.0;		/*!< Maximum current that the OPAs will be limited to on startup */
-const double OPA_R1	= 50;					/*!< OPA gain resistor 1 */
-const double OPA_R2	= 200;					/*!< OPA gain resistor 2 */
+const double OPA_R1	= 40.2;					/*!< OPA gain resistor 1 */
+const double OPA_R2	= 240.2;					/*!< OPA gain resistor 2 */
 const double OPA_GAIN = 
 			(OPA_R1 + OPA_R2) / OPA_R1;		/*!< DAC -> OPA gain, set by on-board resistors. */
 
 // LED output pins
 const uint8_t LED_1 = A5; 					/*!< ATMega pin for LED 1, Arduino labelling */
 const uint8_t LED_2 = A4;					/*!< ATMega pin for LED 2, Arduino labelling */
+
+// Other Microprocessor pins
+const uint8_t DISABLE_SERIAL_CTRL = 0xFF; 	/*!< ATMega pin for disabling serial control: 0 or 5V (set to 0xFF if not present) */
+const uint8_t DIVIDED_SUPPLY_VOLTAGE = 0xFF;/*!< ATMega pin for reading supply voltage: analog voltage (set to 0xFF if not present) */
+const double FALLBACK_SUPPLY_VOLTAGE = 15;	/*!< Assumed supply voltage if DIVIDED_SUPPLY_VOLTAGE pin is not present */
+const double DIVIDED_SUPPLY_FACTOR = 0;		/*!< Factor by which the supply voltage has been divided  */
